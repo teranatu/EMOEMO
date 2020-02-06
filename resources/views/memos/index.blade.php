1eec8226b06cdbc74112c5fafac43086cba80046
teranatu <ranranheim@gmail.com>
2020-02-06 15:41:20 +0700

    1

--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container m-0">
+  <div class="container">
     <a class="navbar-brand " href="/">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
 
-      <!-- navbar左 -->
-      <ul class="navbar-nav mr-auto">
+      <!-- この下の行に mr-auto クラスを付与するだけ -->
+      <ul class="navbar-nav mr-auto">　
         <li class="nav-item">
           <a class="nav-link" href="#">Home</a>
         </li>
@@ -18,19 +18,16 @@
           <a class="nav-link" href="#">About</a>
         </li>
       </ul>
-
-    </div>
-  </div>
       <ul class="navbar-nav">
         <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          username
+          Dropdown
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">色の作成</a>
-          <a class="dropdown-item" href="#">#の作成</a>
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">ログアウト</a>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
         </li>
         <li class="nav-item">
@@ -40,6 +37,9 @@
           <a class="nav-link" href="#">Login</a>
         </li>
       </ul>
+
+    </div>
+  </div>
 </nav>
         <!-- <a href="#" class="btn btn-primary float-right">Username</a>
         <a href="memos/create" class="btn btn-primary float-right">新規作成</a> -->
