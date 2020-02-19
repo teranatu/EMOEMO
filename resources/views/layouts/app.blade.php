de4052738bccfc27d765ffedef44fab9db1f62a0
teranatu <ranranheim@gmail.com>
2020-02-19 18:21:40 +0700

    EMOEMO ver2.2_LP&responsive_fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,77 +22,41 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="https://res.cloudinary.com/dsv09nxlz/image/upload/c_scale,h_44/v1581306872/PROJECT_EMOEMO/EMOEMO_LP/logo_k5jvi5.png" alt="">
-                </a>
-                
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <!-- <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">tweeted</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">aaaaaa</a>
-                        </li>
-                    </ul> -->
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <!-- <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li> -->
-                            <li class="nav-item">
-                            <a class="nav-link" href="/auth/twitter">twitterログイン</a>
-                            </li>
-                            <!-- @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif -->
-                        @else
+<!-- Navigation -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+			<a href="#" class="navbar-brand"><img src="https://res.cloudinary.com/dsv09nxlz/image/upload/c_scale,h_44/v1581306872/PROJECT_EMOEMO/EMOEMO_LP/logo_k5jvi5.png"></a>
+			<button class="navbar-toggler" type="button" data-toggler="collapse" data-target="#navbarResponsive">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+                @guest
+                    <li class="nav-item"><a class="nav-link" href="/auth/twitter">twitterログイン</a></li>
+                @else
+                    <li class="nav-item"><a class="nav-link" href="{{ route('tweeted') }}">ツイート済</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{route('memos.index') }}">全てのメモ</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('memos.create',null ,false) }}">メモる</a></li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+                    </form>
+                    </div>
+                    </li>
+                @endguest
+				</ul>
+			</div>
+		</nav>
+		<!-- End Navigation -->
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('tweeted') }}">ツイート済</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('memos.index') }}">全てのメモ</a>
-                        </li>
-                        <li class="nav-item">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('memos.create',null ,false) }}">メモる</a>
-                        </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                    
-                                
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
         @yield('content')
 
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,188 @@
 @extends('layouts.app')
         
 @section('content')
-<!-- Landing_message -->
-    <div class="headerimage">
-        <p class="top_view1"> 
-            メモしたら<br>
-            そこから呟き<br>
-            あら、便利<br>
-        </p>
-        <p class="top_view2"> 
-            メモアプリ<br>
-            twitterに<br>
-            コピペだるい<br>
-        </p>
-        <p class="top_view3"> 
-            twitter<br>
-            タイムラインを<br>
-            見ただけで<br>
-            気づけばすでに<br>
-            ３０分後<br>
-        </p>
-        <button class="top_btn2" onclick="location.href='/auth/twitter'">
-            <p class="font_recommend">今すぐ使ってみる</p>
-        </button>
+<!-- Start Home Section -->
+    <div id="home">
+
+        <div class="landing">
+            <div class="home-wrap">
+                <div class="back">
+                </div>
+            </div>
+        </div>
+
+        <div class="container caption text-center">
+            <div class="row">
+                <div class=" col-md-3">
+                </div>
+                <div class="col-12 col-md-6">
+                    <h1> あなたのメモには<br>価値がある</h1>
+
+                    <h2 class="color4 pt-4 mb-5">
+                        「
+                        <sapn class="color2">emotional</sapn>
+                        」で「
+                        <span class="color1">easy</span>
+                        」な「
+                        <span class="color3">memo</span>
+                        」
+                    </h2>
+
+                    <a class="btn-2" onclick="location.href='/auth/twitter'">早速始める</a>
+                </div>
+                <div class=" col-md-3">
+                </div>
+            </div>
+        </div>
     </div>
-    
-    <section class="landing_message parent">
-        <div class="color_w">
-            <h2 class="color4">
-                「
-                <sapn class="color1">emotional</sapn>
-                」で「
-                <span class="color2">easy</span>
-                」な「
-                <span class="color3">memo</span>
-                」アプリです。
-            </h2>
-            <p>
-                メモを取り後で編集してツイートする方は多いと思います。<br>
-                その際に、メモアプリからコピペするのでは無くメモを保存しておいて編集できる。<br>
-                そんなアプリです。
-            </p>
+<!-- End Home Section -->
+
+<!-- Start landing -->
+<div class="mtb-6 bgc">
+
+    <!-- Start 機能と利点紹介 -->
+    <!-- End 機能と利点紹介 -->
+<!-- Start landing Section 1 -->
+    <div class="row">
+        <div class="col-3 d-none d-sm-block"></div>
+        <div class="col-sm-12 col-xl-2 ">
+            <img class="bdr card m-0-a bdr_obc"  style="width: 200px; height: 200px;" src="https://res.cloudinary.com/dsv09nxlz/image/upload/v1582103970/PROJECT_EMOEMO/EMOEMO_LP/undraw_share_link_qtxe_xa1_jlulq9.png">
         </div>
-    </section>
-
-<!-- landing_flow -->
-
-    <section class="flow">
-    <div class="container">
-        <div class="row pt-5 pb-5">
-            <div class="col-xl-6 col-md-12 pr-5 pl-5">
-            <img src="https://res.cloudinary.com/dsv09nxlz/image/upload/v1581326185/PROJECT_EMOEMO/EMOEMO_LP/flow_g4z2gu.png" alt="flow" title="flow">
+        <div class="text-center col-sm-12 col-xl-4 text-xl-left">
+            <div class="color_w">
+                <h1 class="color2">
+                    Twitter連携でメモをtweetする機能
+                </h1>
             </div>
-            <div class="col-xl-6 col-md-12 pr-5 pl-5">
-                <div class="color_w text-center">
-                    <h2 class="color1">
-                        ①Twitterでの情報発信＆アウトプットに！
-                    </h2>
-                    <p>
-                        勉強をしてわからない単語をとりあえずメモっておいて後から調べ、
-                        まとめた事をアウトプットとしてEMOEMOからそのまま
-                        twitterへ情報発信できます。
-                    </p>
-                </div>
-                <div class="color_w pt-5">
-                    <h2 class="color2">
-                        ② 情報収集と発信、勉強を切り離せる
-                    </h2>
-                    <p>
-                        情報収集は大切ですが、情報発信の為に開いたTwitterでタイムラインをみてしまっていては元も子もない。
-                        EMOEMOはそれを阻止します！
-                        勉強したいときは勉強。
-                        Twitterで情報収集したりフォロワーと絡んだりする時はTwitterで、情報発信したい時はEMOEMOでメリハリをつけます！
-                    </p>
-                </div>
-                <div class="color_w pt-5">
-                    <h2 class="color3">
-                        ③皆さんの感想お待ちしております！
-                    </h2>
-                    <p>
-                        こんなことができたらいいな！あんなことができたらいいな！等
-                        常にご意見をお待ちしております！ご気軽にこちらからお問い合わせください。
-                    </p>
-                </div>
+            <div class="color_w mt-4">
+                <p>
+                    ・メモアプリで編集&コピーしてTwitterにペースト？<br>
+                    =>EMOEMOで編集し１クリックでtweetし楽チン！<br>
+                <p>
+                <p>
+                    ・画像も送れるの？<br>
+                    =>画像もtweetと一緒に送信可能！(4枚まで)<br>
+                <p>
             </div>
         </div>
+        <div class="col-3 d-none d-sm-block"></div>
+    </div>
+<!-- End landing Section 1 -->
+
+<!-- Start landing Section 2 -->
+    <div class="row mt-5">
+        <div class="col-3 d-none d-sm-block order-0"></div>
+        <div class="text-center col-sm-12 col-xl-4 text-xl-left order-2 order-sm-1">
+            <div class="color_w">
+                <h1 class="color1">
+                よりわかりやすいメモ機能
+                </h1>
+            </div>
+            <div class="color_w mt-4">
+                <p>
+                ・色々なメモアプリがあるけど、機能多すぎて慣れるのが面倒？<br>
+                =>シンプルに作られているのでなんとなくで使えます！
+                </P>
+                <p>
+                ・シンプルすぎて使いにくい？<br>
+                =>tweetする様にメモすることが出来るので、使い易いです！
+                </p>
+            </div>
+        </div>
+        <div class="col-sm-12 col-xl-2 order-1 order-sm-2">
+            <img class="bdr card m-0-a bdr_obc"  style="width: 200px; height: 200px;" src="https://res.cloudinary.com/dsv09nxlz/image/upload/v1582052488/PROJECT_EMOEMO/EMOEMO_LP/undraw_notes1_cf55_1_kriase.png">
+        </div>
+        <div class="col-3 d-none d-sm-block order-3"></div>
+    </div>
+<!-- End landing Section 2 -->
+
+<!-- Start landing Section 3 -->
+    <div class="row mt-5 ">
+        <div class="col-3 d-none d-sm-block"></div>
+        <div class="col-sm-12 col-xl-2">
+            <img class="bdr card m-0-a bdr_obc"  style="width: 200px; height: 200px;" src="https://res.cloudinary.com/dsv09nxlz/image/upload/v1581881405/PROJECT_EMOEMO/EMOEMO_LP/undraw_viral_tweet_gndb_1_uotznf.png">
+        </div>
+        <div class="text-center col-sm-12 col-xl-4 text-xl-left">
+            <div class="color_w">
+                <h1 class="color3">
+                タイムライン見過ぎ防止機能
+                </h1>
+            </div>
+            <div class="color_w mt-4">
+                <p>
+                    情報発信したいけど、タイムラインを見て時間浪費しちゃうんだけど？<br>
+                    =>twitterを開かずにtweetできるので情報収集と発信の切替が容易！
+                <p>
+                <p>
+                ・ツイートを管理できる？<br>
+                =>ツイートしたメモを管理することもできます。
+                </p>
+            </div>
+        </div>
+        <div class="col-3 d-none d-sm-block"></div>
+    </div>
+<!-- End landing Section 3 -->
+</div>
+<!-- End landing -->
+
+<div class="color5 mtb-6">
+<!--  Start how to use  -->
+    <div class="row">
+        <div class="col-3"></div>
+
+        <div class="col-xl-6">
+            <div class="color_w text-center">
+                <h1 class="font-weight-bold">
+                    ＜つかいかた＞
+                </h1>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12">
+                            <h2>STEP1</h2>
+                            <p class="color6">twitterアカウントで<br>ログインする</p>
+                            <div>
+                                <img class="bdr card mt-5 m-0-a"  style="width: 200px; height: 200px;" src="https://res.cloudinary.com/dsv09nxlz/image/upload/v1582055119/PROJECT_EMOEMO/EMOEMO_LP/undraw_Login_v483_1_1_l9sljv.png">
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <h2>STEP2</h2>
+                            <p class="color6">EMOEMOで<br>メモをかく</p>
+                            <div>
+                                <img class="bdr card mt-5 m-0-a"  style="width: 200px; height: 200px;" src="https://res.cloudinary.com/dsv09nxlz/image/upload/v1582055119/PROJECT_EMOEMO/EMOEMO_LP/undraw_Login_v483_1_1_l9sljv.png">
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <h2>STEP3</h2>
+                            <p class="color6">メモから<br>1clickでつぶやく</p>
+                            <div class="text-center">
+                                <img class="bdr card mt-5 m-0-a bdr_obc"  style="width: 200px; height: 200px;" src="https://res.cloudinary.com/dsv09nxlz/image/upload/v1582055119/PROJECT_EMOEMO/EMOEMO_LP/undraw_Login_v483_1_1_l9sljv.png">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row pt-4">
+                        <div class="col-sm-12 color4">
+                            <a class="btn-2" onclick="location.href='/auth/twitter'">早速始める</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3"></div>
+    </div>
+<!-- End how to use -->
+</div>
+
+
+
+<div class="bgc mt-6 pb-4">
+    <div class="color_w text-center color4">
+        <h1>EMOEMO</h1>
+    </div>
+    <div class="color_w text-center color4">
+        <h2>© Tera.</h2>
     </div>
 </div>
+
+
+
 @endsection('content')
