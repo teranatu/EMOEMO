6dce272951fe96af10616046aa6de06e8f4085bb
teranatu <ranranheim@gmail.com>
2020-03-26 17:12:18 +0900

    EMOEMO ver.3.8 SSL-fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
+        
         Schema::defaultStringLength(191);
     }
 }
